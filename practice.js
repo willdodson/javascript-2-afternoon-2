9e7647ba628c8634d0dbf4c4c128487421f93460
wsdodson <wsdodson@gmail.com>
2018-01-23 18:51:59 -0700

    1/2way

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for (let i =0 ; i < family.length; i++){
+    alert(arr[i])
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length-1;i=0;i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
